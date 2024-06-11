c237672da133307e62ee9f2ca9b61d91e31f6a99
Exploser <exploserbusiness@gmail.com>
2024-06-11 19:20:50 -0400

    Update favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import AboutSection from "./components/AboutSection";
 import AchievementsSection from "./components/AchievementsSection";
 import EmailSection from "./components/EmailSection";
+import Footer from "./components/Footer";
 import HeaderSection from "./components/Header";
 import ProjectsSection from "./components/ProjectsSection";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-[#121212] container mx-auto px-12 py-8 ">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-[#121212] container mx-auto px-12 pt-8 pb-0 ">
       <section className="container mt-24 mx-auto px-12 py-4">
         <HeaderSection />
         <AchievementsSection />
@@ -14,6 +15,7 @@ export default function Home() {
         <ProjectsSection />
         <EmailSection />
       </section>
+      <Footer />
     </main>
   );
 }
