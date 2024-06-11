f1950fa8f6af2639301d09155bc125b10aa26730
Exploser <exploserbusiness@gmail.com>
2024-06-11 15:18:51 -0400

    Add about me Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import AboutSection from "./components/AboutSection";
 import HeaderSection from "./components/Header";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-[#121212] container mx-auto px-12 py-8 ">
       <HeaderSection />
+      <AboutSection />
     </main>
   );
 }
