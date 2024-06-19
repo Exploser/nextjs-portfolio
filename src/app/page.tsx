365fc3f94d838a23e66a7c2f4f24bd056d933adf
Exploser <exploserbusiness@gmail.com>
2024-06-18 23:45:24 -0400

    Update Styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import ProjectsSection from "./components/ProjectsSection";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-[#121212] container mx-auto px-12 pt-8 pb-0 ">
-      <section className="container mt-24 mx-auto px-12 py-4">
+      <section className="container mx-auto px-12 py-4">
         <HeaderSection />
         <AchievementsSection />
         <AboutSection />
