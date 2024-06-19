7a8a09af5b33082fbf9a8f7589cb09fa0d85d79d
Exploser <exploserbusiness@gmail.com>
2024-06-18 23:43:41 -0400

    Add more images for projects

--- src/app/components/ProjectCard.tsx
+++ src/app/components/ProjectCard.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import { CodeBracketIcon, EyeIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 
-const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl }: { imgUrl: string, title: string, description: string, gitUrl: string, previewUrl: string }) => {
+const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl }: { imgUrl: Array<string>, title: string, description: string, gitUrl: string, previewUrl: string }) => {
   return (
     <div>
       <div
         className="h-52 md:h-72 rounded-t-xl relative group"
-        style={{ background: `url(${imgUrl})`, backgroundSize: "cover" }}
+        style={{ background: `url(${imgUrl[0]})`, backgroundSize: "cover" }}
       >
         <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500 ">
           <Link
