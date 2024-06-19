2235a12c6877a644c200ae358ef6a597a4013da2
Exploser <exploserbusiness@gmail.com>
2024-06-18 23:58:07 -0400

    Update image

--- src/app/components/ProjectCard.tsx
+++ src/app/components/ProjectCard.tsx
@@ -2,12 +2,31 @@ import React from "react";
 import { CodeBracketIcon, EyeIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 
-const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl }: { imgUrl: Array<string>, title: string, description: string, gitUrl: string, previewUrl: string }) => {
+const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl }: { imgUrl: string, title: string, description: string, gitUrl: string, previewUrl: string }) => {
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 1
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 1
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 1
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  };
   return (
     <div>
       <div
         className="h-52 md:h-72 rounded-t-xl relative group"
-        style={{ background: `url(${imgUrl[0]})`, backgroundSize: "cover" }}
+        style={{ background: `url(${imgUrl})`, backgroundSize: "cover" }}
       >
         <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500 ">
           <Link
