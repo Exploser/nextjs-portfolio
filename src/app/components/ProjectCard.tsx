ad5928caa6abf9442c883a887f4267d7b43f89ae
Exploser <exploserbusiness@gmail.com>
2024-06-18 23:58:33 -0400

    Remove dead code

--- src/app/components/ProjectCard.tsx
+++ src/app/components/ProjectCard.tsx
@@ -3,25 +3,6 @@ import { CodeBracketIcon, EyeIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 
 const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl }: { imgUrl: string, title: string, description: string, gitUrl: string, previewUrl: string }) => {
-  const responsive = {
-    superLargeDesktop: {
-      // the naming can be any, depends on you.
-      breakpoint: { max: 4000, min: 3000 },
-      items: 1
-    },
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 1
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 1
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1
-    }
-  };
   return (
     <div>
       <div
