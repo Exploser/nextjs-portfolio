3ff7c55bbf1146caf570b672348cdc6e31b56f0e
Exploser <exploserbusiness@gmail.com>
2024-06-21 12:24:53 -0400

    Add svgs for tools

--- src/app/components/AboutSection.tsx
+++ src/app/components/AboutSection.tsx
@@ -8,13 +8,18 @@ const TAB_DATA = [
     title: "Skills",
     id: "skills",
     content: (
-      <ul className="list-disc pl-2">
-        <li>Node.js</li>
-        <li>Express</li>
-        <li>PostgreSQL</li>
-        <li>Sequelize</li>
-        <li>JavaScript</li>
-        <li>React</li>
+      <ul className="pl-2" id="skills">
+        <li>
+          <img src="/images/tools/node-js.svg"></img>
+          <span className="tooltip">Node.js</span>
+        </li>
+        <li><img src="/images/tools/express-js.svg"></img></li>
+        <li><img src="/images/tools/postgres-sql.svg"></img></li>
+        <li><img src="/images/tools/firebase.svg"></img></li>
+        <li><img src="/images/tools/javascript.svg"></img></li>
+        <li><img src="/images/tools/react.svg"></img></li>
+        <li><img src="/images/tools/c-sharp.svg"></img></li>
+        <li><img src="/images/tools/next-js.svg"></img></li>
       </ul>
     ),
   },
@@ -51,9 +56,9 @@ const AboutSection = () => {
   };
 
   return (
-    <section className="text-white" id="about">
+    <section className="text-white min-h-fit" id="about">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image alt='About Me' src="/images/about-image.png" width={500} height={500} />
+        <Image alt='About Me' src="/images/header-img2.svg" width={500} height={500} />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
@@ -64,7 +69,7 @@ const AboutSection = () => {
             looking to expand my knowledge and skill set. I am a team player and
             I am excited to work with others to create amazing applications.
           </p>
-          <div className="flex flex-row justify-start mt-8">
+          <div className="flex flex-row justify-between mt-8">
             <TabButton
               selectTab={() => handleTabChange("skills")}
               active={tab === "skills"}
