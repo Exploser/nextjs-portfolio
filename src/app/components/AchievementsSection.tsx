7a526527fb38a24b1f780a47afeb7f50f3c93b28
Exploser <exploserbusiness@gmail.com>
2024-06-22 17:46:32 -0400

    Add Github Calendar

--- src/app/components/AchievementsSection.tsx
+++ src/app/components/AchievementsSection.tsx
@@ -1,59 +1,136 @@
 "use client";
-import React from "react";
+
+import React, { useState, useEffect, useRef } from "react";
 import dynamic from "next/dynamic";
+import GitHubCalendar from 'react-github-calendar';
 
-const AnimatedNumbers = dynamic(
-  () => {
-    return import("react-animated-numbers");
-  },
-  { ssr: false }
-);
+interface GitHubUser {
+  login: string;
+  id: number;
+  node_id: string;
+  avatar_url: string;
+  gravatar_id: string;
+  url: string;
+  html_url: string;
+  followers_url: string;
+  following_url: string;
+  gists_url: string;
+  starred_url: string;
+  subscriptions_url: string;
+  organizations_url: string;
+  repos_url: string;
+  events_url: string;
+  received_events_url: string;
+  type: string;
+  site_admin: boolean;
+  name: string;
+  company: string | null;
+  blog: string;
+  location: string | null;
+  email: string | null;
+  hireable: boolean | null;
+  bio: string;
+  twitter_username: string;
+  public_repos: number;
+  public_gists: number;
+  followers: number;
+  following: number;
+  created_at: string;
+  updated_at: string;
+}
 
-const achievementsList = [
-  {
-    metric: "Projects",
-    value: "10",
-    postfix: "+",
-  },
-  {
-    prefix: "~",
-    metric: "Users",
-    value: "300",
-  },
-  {
-    metric: "Awards",
-    value: "7",
-  },
-  {
-    metric: "Years",
-    value: "5",
-  },
-];
+const AnimatedNumbers = dynamic(() => import("react-animated-numbers"), {
+  ssr: false,
+});
 
 const AchievementsSection = () => {
+  const [profilePicture, setProfilePicture] = useState("");
+  const [username, setUsername] = useState("exploser");
+  const [achievementsList, setAchievementsList] = useState([
+    {
+      metric: "Projects",
+      value: "10",
+      postfix: "+",
+    },
+    {
+      prefix: "~",
+      metric: "Users",
+      value: "300",
+    },
+    {
+      metric: "Awards",
+      value: "7",
+    },
+    {
+      metric: "Gists",
+      value: "5",
+    },
+  ]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/exploser", {
+      method: "GET",
+    })
+      .then((response) => response.json())
+      .then((data: GitHubUser) => {
+        // Update the achievements list with GitHub data
+        setAchievementsList([
+          {
+            metric: "Projects",
+            value: data.public_repos.toString(),
+            postfix: "+",
+          },
+          {
+            prefix: "~",
+            metric: "Followers",
+            value: data.followers.toString(),
+          },
+          {
+            metric: "Following",
+            value: data.following.toString(),
+          },
+          {
+            metric: "Gists",
+            value: data.public_gists.toString(),
+          },
+        ]);
+        setProfilePicture(data.avatar_url);
+        setUsername(data.login);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }, []);
+
   return (
-    <div className="py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+    <div className="py-8 px-4 xl:gap-16 sm:py-16 xl:px-16 w-full">
+      <span className="text-white text-2xl font-bold pb-4">My GitHub Contributions,</span>
+      <div className="flex justify-center items-center bg-gray-800 p-4 sm:border-[#33353F] rounded-md mt-4">
+        <div className="text-center mr-4">
+          <img src={profilePicture} className="w-32 h-32 rounded-full"></img>
+          <span className="text-white text-2xl font-bold pt-2">{username}</span>
+        </div>
+        <GitHubCalendar username="exploser" colorScheme="dark" />
+      </div>
       <div className="sm:border-[#33353F] sm:border rounded-md py-8 px-16 flex flex-col sm:flex-row items-center justify-between">
-        {achievementsList.map((achievement, index) => {
-          return (
-            <div
-              key={index}
-              className="flex flex-col items-center justify-center mx-4 my-4 sm:my-0"
-            >
-              <h2 className="text-white text-4xl font-bold flex flex-row">
-                {achievement.prefix}
-                <AnimatedNumbers
-                  includeComma
-                  animateToNumber={parseInt(achievement.value)}
-                  locale="en-US"
-                  className="text-white text-4xl font-bold"
-                />
-                {achievement.postfix}
-              </h2>
-              <p className="text-[#ADB7BE] text-base">{achievement.metric}</p>
-            </div>
-          );
-        })}
+        {achievementsList.map((achievement, index) => (
+          <div
+            key={index}
+            className="flex flex-col items-center justify-center mx-4 my-4 sm:my-0"
+          >
+            <h2 className="text-white text-4xl font-bold flex flex-row">
+              {achievement.prefix}
+              <AnimatedNumbers
+                includeComma
+                animateToNumber={parseInt(achievement.value)}
+                locale="en-US"
+                className="text-white text-4xl font-bold"
+              />
+              {achievement.postfix}
+            </h2>
+            <p className="text-[#ADB7BE] text-base">{achievement.metric}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
