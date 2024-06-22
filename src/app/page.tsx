43860fe48440db920d20caa4f603cdc2d4a4221a
Exploser <exploserbusiness@gmail.com>
2024-06-22 13:28:12 -0400

    Update responsive design for main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,8 @@ import ProjectsSection from "./components/ProjectsSection";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-[#121212] container mx-auto px-12 pt-8 pb-0 ">
-      <section className="container mx-auto px-12 py-4">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-[#121212] container px-12 pt-8 pb-0 xs:mx-2 sm:mx-auto md:mx-auto lg:mx-auto xl:mx-auto  xs:px-4">
+      <section className="container xs:mx-2 sm:mx-auto md:mx-auto lg:mx-auto xl:mx-auto px-12 py-4">
         <HeaderSection />
         <AchievementsSection />
         <AboutSection />
