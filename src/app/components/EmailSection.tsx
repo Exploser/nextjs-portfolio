b0071d33604c5b16f2d52e12b9a333955e828a5f
Exploser <exploserbusiness@gmail.com>
2024-06-25 15:38:38 -0400

    Add text animations to Emails Section

--- src/app/components/EmailSection.tsx
+++ src/app/components/EmailSection.tsx
@@ -113,8 +113,8 @@ const EmailSection: React.FC = () => {
     >
       <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
       <div className="z-10">
-        <h5 className="text-xl font-bold text-white my-2">Let&apos;s Connect</h5>
-        <p className="text-[#ADB7BE] mb-4 max-w-md">
+        <span className="text-white text-2xl font-bold pb-4 text-animation-small my-2 mb-2">Connect with Me,</span>
+        <p className="text-[#ADB7BE] m-4 max-w-md">
           I&apos;m currently looking for new opportunities, my inbox is always open.
           Whether you have a question or just want to say hi, I&apos;ll try my best
           to get back to you!
@@ -128,7 +128,7 @@ const EmailSection: React.FC = () => {
           </Link>
         </div>
       </div>
-      <div>
+      <div className="my-8">
         {emailSubmitted ? (
           <p className="text-green-500 text-sm mt-2">Email sent successfully!</p>
         ) : (
