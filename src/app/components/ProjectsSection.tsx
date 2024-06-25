c6678eae29621e97d8c1e7b181e166fc43dfcc1d
Exploser <exploserbusiness@gmail.com>
2024-06-25 15:39:05 -0400

    Add text animations to projects Section

--- src/app/components/ProjectsSection.tsx
+++ src/app/components/ProjectsSection.tsx
@@ -70,8 +70,8 @@ const ProjectsSection = () => {
 
   return (
     <section id="projects">
-      <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
-        My Projects
+      <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12 text-animation-small">
+        Things that I have made,
       </h2>
       <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
         <ProjectTag
