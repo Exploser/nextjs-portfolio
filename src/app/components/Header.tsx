b21f108b0cdcf59c5d271bc59f3cde88b5c4a3a9
Exploser <exploserbusiness@gmail.com>
2024-06-25 18:28:22 -0400

    Update dual mode ui for header section

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -4,11 +4,13 @@ import Image from "next/image";
 import { TypeAnimation } from "react-type-animation";
 import { motion } from "framer-motion";
 import Link from "next/link";
-
+import { useDarkMode } from "../context/DarkModeContext";
 
 const HeaderSection = () => {
+  const { darkMode } = useDarkMode();
+
   return (
-    <section className="lg:py-8 w-full">
+    <section className={`lg:py-8 w-full ${darkMode ? ' text-white' : ' text-slate-800'}`}>
       <div className="flex flex-col sm:flex-row items-center text-center">
         <motion.div
           initial={{ opacity: 0, scale: 0.5 }}
@@ -16,12 +18,12 @@ const HeaderSection = () => {
           transition={{ duration: 0.5 }}
           className="flex-1 text-center sm:text-left"
         >
-          <h1 className="text-white mb-4 text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-8xl lg:leading-normal font-extrabold xs:min-h-[165px] min-w-full">
+          <h1 className="mb-4 text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-8xl lg:leading-normal font-extrabold xs:min-h-[165px] min-w-full">
             <span className="text-l sm:text-xl md:text-3xl lg:text-4xl xl:text-6xl block">
               Hello,
             </span>
             <span className="block w-full mt-8 lg:mt-12 md:mt-6 sm:mt-6">
-              I&apos;m<span className="text-animation"> Abhijeet</span>
+              I&apos;m<span className={`${darkMode? 'text-animation' : 'text-slate-800'}`}> Abhijeet</span>
             </span>
             <div className="flex items-start justify-start w-full sm:h-[100px] md:h-[100px] lg:h-[250px] mt-8 lg:mt-12 md:mt-6 sm:mt-6 xs:mt-6">
               <TypeAnimation
@@ -44,7 +46,7 @@ const HeaderSection = () => {
               />
             </div>
           </h1>
-          <p className="text-[#ADB7BE] text-base xs:text-xs sm:text-lg mb-6 lg:text-xl mt-8 w-full">
+          <p className={`text-base xs:text-xs sm:text-lg mb-6 lg:text-xl mt-8 w-full ${darkMode ? 'text-[#ADB7BE]' : 'text-[#6B7280]'}`}>
             Specializing in writing clean, decoupled, and testable code. Skilled in Object-Oriented Programming and versed in Agile and SCRUM methodologies. Committed to continuous learning.
           </p>
           <div>
@@ -54,14 +56,6 @@ const HeaderSection = () => {
             >
               Connect with Me
             </Link>
-            {/* <Link
-              href="/"
-              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
-            >
-              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
-                Download CV
-              </span>
-            </Link> */}
           </div>
         </motion.div>
         <motion.div
