46e5ba917eb8f527a72da31f81eba760741fcbef
Exploser <exploserbusiness@gmail.com>
2024-07-12 00:09:56 -0400

    Update responsive design for Footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="footer w-screen border-t text-white">
-      <div className="container mx-auto p-6 flex flex-col md:flex-row justify-between items-center">
+      <div className="container mx-auto p-6 sm:p-0 xs:p-0 flex flex-col md:flex-row justify-between items-center">
         <Image src="/logo.svg" alt="Exploser Logo" width={60} height={60} />
         <p className="text-slate-400 mt-4 md:mt-0">Icons by @<a className="underline" href="https://www.svgrepo.com/">SVGRepo</a> </p>
       </div>
